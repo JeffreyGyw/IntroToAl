5f75e3682acb45b457bcc71f4c8763c56d6ff559
guyiwei <1970471638@qq.com>
2015-06-27 15:55:30 +0800

    implement insert sort a

--- src/chapter2/InsertSortA.java
+++ src/chapter2/InsertSortA.java
@@ -1,5 +1,23 @@
 package chapter2;
 
-public class InsertSortA {
+import java.util.Arrays;
 
+public class InsertSortA {
+	public static int[] insertsorta(int[] s){
+		int temp = 0;
+		for(int j=1;j<s.length;j++){
+			temp = s[j];
+			int i = j-1;
+			while( i>0 && s[i]>temp){
+				s[i+1] = s[i];
+				i--;
+			}
+			s[i+1] = temp;
+		}
+		return s;
+	}
+	public static void main(String[] args){
+		int[] s = {1,2,5,9,3,2,4};
+		System.out.println(Arrays.toString(insertsorta(s)));
+	}
 }
