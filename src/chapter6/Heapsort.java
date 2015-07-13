164352be7f009fd5620fc9d8209eca4ae262c143
guyiwei <1970471638@qq.com>
2015-07-13 20:48:57 +0800

    better heapsort

--- src/chapter6/Heapsort.java
+++ src/chapter6/Heapsort.java
@@ -1,61 +1,56 @@
 package chapter6;
 
-import java.util.Arrays;
-
 public class Heapsort {
-	public static int parent(int i) {
-		return (i-1)/2;
-	}
-	public static int left(int i) {
-		return (i+1)*2-1;
-	}
-	public static int right(int i) {
-		return (i+1)*2;
-	}
-	public static void maxheapify(int[] a, int i) {
-		int l = left(i);
-		int r = right(i);
-		int largest = 0;
-		int temp = 0;
-		if(l<a.length&&a[l]>a[i]){
-			largest = l;
-		}else {
-			largest = i;
-		}
-		if(r<a.length&&a[r]>a[largest]){
-			largest = r;
-		}
-		if(largest!=i){
-			temp = a[i];
-			a[i] = a[largest];
-			a[largest] = temp;
-			maxheapify(a, largest);
-		}
-	}
-	public static void buildmaxheap(int[] a) {
-		for(int i = (a.length-1)/2;i>=0;i--){
-			maxheapify(a, i);
-		}
-	}
-	public static void heapsort(int[] a) {
-		buildmaxheap(a);
-		int[] b = new int[a.length];
-		System.arraycopy(a, 0, b, 0, a.length);
-		int temp;
-		for(int i = a.length-1;i>=1;i--){
-			temp = b[i];
-			b[i] = b[0];
-			b[0] = temp;
-			a[i] = b[i];
-			a[0] = b[0];
-			b = new int[i];
-			System.arraycopy(a,0, b, 0, i);
-			maxheapify(b, 0);
-		}
-	}
-	public static void main(String[] args) {
-		int[] a = {4,1,3,2,16,9,10,14,8,7};
-		heapsort(a);
-		System.out.println(Arrays.toString(a));
-	}
+
+    public static void sort(Comparable[] data) {
+        // 构建最大堆
+        buildMaxHeap(data);
+        // 循环，每次把根节点和最后一个节点调换位置
+        for (int i = data.length; i > 1; i--) {
+            Comparable tmp = data[0];
+            data[0] = data[i - 1];
+            data[i - 1] = tmp;
+
+            // 堆的长度减少1，排除置换到最后位置的根节点
+            maxHeapify(data, 1, i - 1);
+        }
+    }
+
+    // 根据输入数组构建一个最大堆
+    private static void buildMaxHeap(Comparable[] data) {
+        for (int i = data.length / 2; i > 0; i--) {
+            maxHeapify(data, i, data.length);
+        }
+    }
+
+    //堆调整，使其生成最大堆
+    private static void maxHeapify(Comparable[] data, int parentNodeIndex, int heapSize) {
+        // 左子节点索引
+        int leftChildNodeIndex = parentNodeIndex * 2;
+        // 右子节点索引
+        int rightChildNodeIndex = parentNodeIndex * 2 + 1;
+        // 最大节点索引
+        int largestNodeIndex = parentNodeIndex;
+
+        // 如果左子节点大于父节点，则将左子节点作为最大节点
+        if (leftChildNodeIndex <= heapSize && data[leftChildNodeIndex - 1].compareTo(data[parentNodeIndex - 1]) > 0) {
+            largestNodeIndex = leftChildNodeIndex;
+        }
+
+        // 如果右子节点比最大节点还大，那么最大节点应该是右子节点
+        if (rightChildNodeIndex <= heapSize && data[rightChildNodeIndex - 1].compareTo(data[largestNodeIndex - 1]) > 0) {
+            largestNodeIndex = rightChildNodeIndex;
+        }
+
+        // 最后，如果最大节点和父节点不一致，则交换他们的值
+        if (largestNodeIndex != parentNodeIndex) {
+            Comparable tmp = data[parentNodeIndex - 1];
+            data[parentNodeIndex - 1] = data[largestNodeIndex - 1];
+            data[largestNodeIndex - 1] = tmp;
+
+            // 交换完父节点和子节点的值，对换了值的子节点检查是否符合最大堆的特性
+            maxHeapify(data, largestNodeIndex, heapSize);
+        }
+    }
+
 }
